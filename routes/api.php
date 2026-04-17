a51ca8509349a20f25445423691196145327c24e
Nur Siti Aisyah <nur617859@gmail.com>
2026-04-17 21:11:41 +0700

    feat auth

---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\Api\V1\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use PHPUnit\Framework\Attributes\Group;
+
+Route::prefix('v1')->group(function () {
+    Route::post('/login', [AuthController::class, 'login']);
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('/me', [AuthController::class, 'me']);
+        Route::post('/logout', [AuthController::class, 'logout']);
+    });
+});
