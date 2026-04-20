29ef14f399ef17890c60e58447a886cc6e337e28
Nur Siti Aisyah <nur617859@gmail.com>
2026-04-20 18:16:24 +0700

    feat product

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\Api\V1\AuthController;
 use App\Http\Controllers\Api\V1\ProductCategoryController;
 use App\Http\Controllers\Api\V1\ProductCategoryImageController;
+use App\Http\Controllers\Api\V1\ProductController;
+use App\Http\Controllers\Api\V1\ProductImageController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use PHPUnit\Framework\Attributes\Group;
@@ -17,5 +19,8 @@ Route::prefix('v1')->group(function () {
         Route::get('product-categories/options', [ProductCategoryController::class, 'options']);
         Route::post('product-categories/{id}/image', [ProductCategoryImageController::class, 'store']);
         Route::apiResource('product-categories', ProductCategoryController::class);
+
+        Route::post('products/{id}/image', [ProductImageController::class, 'store']);
+        Route::apiResource('products', ProductController::class);
     });
 });
